5f97009a4e6c9dcc171f8aca20921e1a3048dac8
Yasodha Sundaresan <yasodhasundaresan@Yasodhas-MBP.attlocal.net>
2019-10-17 16:06:55 -0400

    Fiirst commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+
+import {Employee}  from './Employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyNewApp';
+export class AppComponent implements OnInit,AfterViewInit{
+
+  sum:number;
+  title:string = null;
+  flag:boolean;
+  arr:Array<string>;
+  empArr:Array<Employee>;
+  //randomVar:any;
+  image:string;
+  name:string;
+  
+  constructor(){
+    
+  }
+
+  @ViewChild('content',{static:false}) randomVar:ElementRef;
+
+  @ViewChildren('cities') city:QueryList<string>;
+
+  ngOnInit(){
+    console.log('app comp initialized');
+    this.sum=this.addNumbers(10,20);
+    console.log("TCL: AppComponent -> constructor ->  this.sum",  this.sum)
+    this.title='Chicago';
+    this.flag=false;
+    this.name='Lei';
+    this.image='../assets/Bitcoin-Landing-page-Design-Template-psd-950x1333.jpg';
+
+    this.empArr=[
+      {
+      name:'John',
+      age:30
+      },
+      {
+        name:'Alex',
+        age:35
+      },
+      {
+        name:'Albert',
+        age:25
+      }
+    ]
+   }
+
+    ngAfterViewInit(){
+      console.log('After view init',this.randomVar.nativeElement.innerHTML);
+      console.log(this.city);
+    }
+  
+    addNumbers(a:number,b:number):number{
+      return a+b;
+    }
+
+
+  save(){
+    console.log('You clicked the button');
+    this.flag=!this.flag;
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightsComponent } from './flights/flights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightsComponent
   ],
   imports: [
     BrowserModule,
